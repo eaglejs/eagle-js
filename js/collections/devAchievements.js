3d501e763e556197e74764a353ffccb3fd0316fe
eaglejs <joshuaseagle@gmail.com>
2018-12-04 10:12:04 -0500

    updated the url for teamtreehouse

--- js/collections/devAchievements.js
+++ js/collections/devAchievements.js
@@ -1,11 +1,11 @@
 define (function (require) {
     'use strict';
-    
+
     var devAchievement = require("models/devAchievement");
-    
+
     return Backbone.Collection.extend({
         model: devAchievement,
-        url: "https://teamtreehouse.com/joshuaeagle.json",
+        url: "https://teamtreehouse.com/eaglejs.json",
         parse: function (response) {
             return response.badges;
         },
